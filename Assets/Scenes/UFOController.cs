7e264b716d832b4efb3023408521fdb6262ef615
Dongseon Yang <ydsperfect08@ewhain.net>
2025-04-08 08:14:17 +0900

    feat : UFO controller 주석 추가

--- Assets/Scenes/UFOController.cs
+++ Assets/Scenes/UFOController.cs
@@ -20,8 +20,9 @@ public class UFOController : MonoBehaviour
     {
         float moveX = Input.GetAxisRaw("Horizontal"); // A, D
         float moveZ = Input.GetAxisRaw("Vertical");   // W, S
-        Vector3 move = new Vector3(moveX, 0f, moveZ).normalized;
-
+        Vector3 move = new Vector3(moveX, 0f, moveZ).normalized; //normalized로 이동속도가 일정하게 유지되도록 함
+    
+        // rb.MovePosition(...) -> 부드럽고 충돌 가능하게 이동시켜주는 함수, 벽에 부딪혀도 충돌 작동
         rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
    
     }
