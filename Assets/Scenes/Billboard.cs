5f767df320023a8cd2cc4fa0969d86650e93045e
Dongseon Yang <ydsperfect08@ewhain.net>
2025-04-08 13:18:09 +0900

    fix : UFO iskinetic=true로 수정

--- Assets/Scenes/Billboard.cs
+++ Assets/Scenes/Billboard.cs
@@ -6,6 +6,8 @@ public class Billboard : MonoBehaviour
 {
     void LateUpdate()
     {
-        transform.forward = Camera.main.transform.forward;
+        Vector3 lookDir = Camera.main.transform.position - transform.position;
+        lookDir.y = 0; // 수직 축 무시
+        transform.forward = -lookDir.normalized;
     }
 }
