1f2794130f38e1065120443224331c7e9c0cc7bc
Dongseon Yang <ydsperfect08@ewhain.net>
2025-05-09 01:38:51 +0900

    feat : human 인식 script 추가

---
+++ Assets/Scenes/Human.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    public int scoreValue = 10; // 기본값은 10, B타입은 20으로 설정
+}
