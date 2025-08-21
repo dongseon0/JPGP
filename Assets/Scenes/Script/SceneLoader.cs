93fd02538a9b0991eef3aa2bf0685e33c1ee3217
Dongseon Yang <ydsperfect08@ewhain.net>
2025-08-22 01:08:24 +0900

    Feat : exe로 빌딩

--- Assets/Scenes/Script/SceneLoader.cs
+++ Assets/Scenes/Script/SceneLoader.cs
@@ -5,7 +5,18 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 1f;  
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;  
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+#else
+        Application.Quit(); // 게임 종료
+#endif
     }
 }
